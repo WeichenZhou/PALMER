12544c36fcde42e1f48f74fd6fd481203bc9bb60
Weichen Zhou <arthurzhou10@gmail.com>
2018-02-13 17:14:49 -0500

    Add files via upload
    Beta Ver1.1. Add modules of Alu and SVA. Available for Reference GRCh37 and 38. Fix minor bugs. Ready for "MAKE" and "QUICK START".

---
+++ scp/blastn.cpp
@@ -0,0 +1,49 @@
+//copyright by ArthurZhou @ UMich&FUDAN&HUST
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <sstream>
+#include <algorithm>
+#include <functional>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+int blastn(string WD, string WD_dir, string t){
+    
+    //cout<<"Blastn Step is now running."<<endl;
+    
+    string sys_blast = WD_dir+"SEQ.masked";
+    char *syst_blast =  new char[sys_blast.length()+1];
+    strcpy(syst_blast, sys_blast.c_str());
+    
+    ifstream file1;
+    file1.open(syst_blast);
+    
+    if (!file1.is_open())
+    {
+        cout <<"CANNOT OPEN FILE"<< endl;
+        exit(1);
+    }
+    string sys_blastn;
+    if(t=="LINE"){
+         sys_blastn = "blastn -evalue 0.001 -task blastn -gapopen 4 -query lib/L1.3.fasta -subject "+WD_dir+"SEQ.masked -outfmt \"7 qacc sacc evalue qstart qend sstart send\" >  "+WD_dir+"blastn.txt";
+    }
+    else if(t=="ALU"){
+        sys_blastn = "blastn -evalue 0.001 -task blastn -gapopen 4 -query lib/AluY.fasta -subject "+WD_dir+"SEQ.masked -outfmt \"7 qacc sacc evalue qstart qend sstart send\" >  "+WD_dir+"blastn.txt";
+    }
+    else if(t=="SVA"){
+        sys_blastn = "blastn -evalue 0.001 -task blastn -gapopen 4 -query lib/SVA_F.fasta -subject "+WD_dir+"SEQ.masked -outfmt \"7 qacc sacc evalue qstart qend sstart send\" >  "+WD_dir+"blastn.txt";
+    }
+    
+    char *syst_blastn = new char[sys_blastn.length()+1];
+    strcpy(syst_blastn, sys_blastn.c_str());
+    
+    system(syst_blastn);
+}
